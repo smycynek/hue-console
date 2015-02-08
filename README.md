c48816f58bc59a020408b03a6e8ae2d99f87ae12
Steve Victor <sv@stevenvictor.net>
2015-02-07 20:29:57 -0500

    add debug modes, add router button

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 Hue Console
 ===============
 
-*  A simple single-page app to demonstrate the Philips Hue API.
-*  Require a local Philips Hue installation
-   *   Node users, just use 'npm install' and './bin/www'
+*  A simple AngularJS app to demonstrate the Philips Hue API. ( http://www.developers.meethue.com/ )
+*  Uses jshue - https://github.com/blargoner/jshue (MIT License)
+*  Require a local Philips Hue installation or a hue emulator, such as
+   https://www.npmjs.com/package/hue-simulator
+*  Node users, just use 'npm install' and './bin/www'
 
--- public/hueConsole/js/hueComands.js
+++ public/hueConsole/js/hueComands.js
@@ -1,4 +1,5 @@
 /*global alert */
+/*global confirm */
 /*global angular */
 /*global jsHue */
 
@@ -33,23 +34,69 @@ var miredToKelvin = function(mired) {
 var hueNgApp = angular.module("hueNgApp", []);
 
 //Main (and only) angular controller for the body of index.html
-hueNgApp.controller("HueCtrl", function ($scope) {
+hueNgApp.controller("HueCtrl", function($scope) {
     "use strict";
     $scope.debug = false;
     $scope.hueApi = jsHue();
-    $scope.hueBridgeIp = "192.168.1.106";
+    $scope.color = 0;
+    $scope.debugHexColor = "#0f0";
+    $scope.hueBridgeIp = "localhost";
     $scope.hueClientId = "newdeveloper";
-    $scope.isDisabled  = true;
+    $scope.isDisabled = true;
     $scope.lightCount = 0;
     $scope.lightList = [];
     $scope.colorTemperature = 4000;
     $scope.brightness = 180;
     $scope.hueValue = 0;
     $scope.saturation = 128;
+    $scope.currentValueStyle = "color:black;text-decoration:unset";
+    $scope.dirtyValueStyle = "color:red;text-decoration:line-through";
+    $scope.temperatureDirtyState = $scope.currentValueStyle;
+    $scope.hueSaturationDirtyState = $scope.currentValueStyle;
+    $scope.debugAlert = function(caption, data) {
+        if ($scope.debug) {
+            alert("Debug: " + caption + " Data: " + JSON.stringify(data));
+        }
+    };
+
+    $scope.findBridge = function() {
+        var errMessage = "Could not find hue bridge: Try:  \n1) Setting IP manually\n2) Use 'localhost' \n3) See Hue developer documentation.\n",
+            success = function(data) {
+                try {
+                    $scope.debugAlert("findBridge", data);
+                    var ip = data[0].internalipaddress;
+
+                    if (confirm("Found bridge IP " + ip + ".  Use to connect?")) {
+                        $scope.hueBridgeIp = ip;
+                        $scope.$apply();
+                        $scope.reconnect();
+                    } else {
+                        return;
+                    }
+                } catch (err) {
+                    alert(errMessage + ", Error Data: " + JSON.stringify(err));
+                }
+            },
+            failure = function(data) {
+                alert(errMessage + ", Error Data: " + JSON.stringify(data));
+            };
+        $scope.hueApi.discover(success, failure);
+
+    };
 
     $scope.initHue = function() {
         $scope.user = $scope.hueApi.bridge($scope.hueBridgeIp).user($scope.hueClientId);
     };
+
+
+//Create a new color with RGB values scaled by a factor.
+
+
+    $scope.setColor = function() {
+        var hexColor = $scope.formatInt($scope.color.toString(16), 6);
+        $scope.debugHexColor = "#" + hexColor;
+    };
+
     $scope.formatInt = function(intValue, totalSize) {
         var formattedString = String(intValue);
         while (formattedString.length < totalSize) {
@@ -61,66 +108,85 @@ hueNgApp.controller("HueCtrl", function ($scope) {
     $scope.reconnect = function() {
         $scope.lightList = ["Initializing..."];
         $scope.updating = "(Updating...)";
-        $scope.isDisabled =  true;
+        $scope.isDisabled = true;
         $scope.initHue();
         $scope.getLights();
     };
 
     $scope.mainControl = function() {
-        $scope.reconnect();
+        $scope.debugAlert("mainControl", "Debug mode: " + $scope.debug);
     };
 
     $scope.getCurrentSettings = function() {
         var success = function(data) {
-            var brightness = Number(data.state.bri), ctMired = Number(data.state.ct), hue = Number(data.state.hue), sat = Number(data.state.sat);
+            $scope.debugAlert("getCurrentSettings", data);
+            var brightness = Number(data.state.bri),
+                ctMired = Number(data.state.ct),
+                hue = Number(data.state.hue),
+                sat = Number(data.state.sat);
             $scope.brightness = brightness;
             $scope.colorTemperature = miredToKelvin(ctMired);
             $scope.hueValue = hue;
             $scope.saturation = sat;
             $scope.updating = "";
             $scope.$apply();
-        }, failure = function(data) {
-            alert("Error getting lighting settings: " + JSON.stringify(data));
-            $scope.isDisabled = true;
-        };
-        $scope.user.getLight(1, success, failure);  //sample settings off light with ID 1 for now.
+        },
+            failure = function(data) {
+                alert("Error getting lighting settings: " + JSON.stringify(data));
+                $scope.isDisabled = true;
+            };
+        $scope.user.getLight(1, success, failure); //sample settings off light with ID 1 for now.
     };
 
     $scope.getLights = function() {
-        var lightData = [], success = function(data) {
-            var prop;
-            for (prop in data) {
-                if (data.hasOwnProperty(prop)) {
-                    lightData.push(data[prop].name);
+        var lightData = [],
+            success = function(data) {
+                $scope.debugAlert("getLights", data);
+                var prop;
+                for (prop in data) {
+                    if (data.hasOwnProperty(prop)) {
+                        lightData.push(data[prop].name);
+                    }
                 }
-            }
-            $scope.lightList = lightData;
-            $scope.lightCount = lightData.length;
-            $scope.isDisabled = false;
-            $scope.$apply();
-            $scope.getCurrentSettings();
-        }, failure = function(data) {
-            $scope.lightCount = -1;
-            $scope.lightList = ["Error getting light data: ", JSON.stringify(data)];
-            $scope.isDisabled = true;
-        }, calledLights = $scope.user.getLights(success, failure);
+                $scope.lightList = lightData;
+                $scope.lightCount = lightData.length;
+                $scope.isDisabled = false;
+                $scope.$apply();
+                $scope.getCurrentSettings();
+            },
+            failure = function(data) {
+                $scope.lightCount = -1;
+                $scope.lightList = ["Error getting light data: ", JSON.stringify(data)];
+                $scope.isDisabled = true;
+            },
+            calledLights = $scope.user.getLights(success, failure);
         return calledLights;
     };
 
     $scope.setAll = function(client, data, totalLights) {
-        var id;
+        var id, success = function(data) {
+            $scope.debugAlert("setAll", data);
+        }, failure = function(data) {
+            alert("Error setting lights: " + JSON.stringify(data));
+        };
         for (id = 1; id !== totalLights + 1; id = id + 1) {
-            client.setLightState(id, data);
+            client.setLightState(id, data, success, failure);
         }
     };
 
     $scope.setOnOff = function(state) {
-        $scope.setAll($scope.user, {on: state}, $scope.lightCount);
+        $scope.setAll($scope.user, {
+            on: state
+        }, $scope.lightCount);
     };
 
     $scope.setCustomTemperature = function(kelvinValue) {
         var mired = kelvinToMired(kelvinValue);
-        $scope.setAll($scope.user, {ct : mired}, $scope.lightCount);
+        $scope.setAll($scope.user, {
+            ct: mired
+        }, $scope.lightCount);
+        $scope.setHueSaturationDirty();
+        $scope.apply();
     };
 
     $scope.setRed = function() {
@@ -137,31 +203,50 @@ hueNgApp.controller("HueCtrl", function ($scope) {
 
     $scope.setBrightness = function(val) {
         $scope.brightness = val;
-        $scope.setAll($scope.user, {bri : $scope.brightness}, $scope.lightCount);
+        $scope.setAll($scope.user, {
+            bri: $scope.brightness
+        }, $scope.lightCount);
+    };
+
+    $scope.setTemperatureDirty = function() {
+        $scope.hueSaturationDirtyState = $scope.currentValueStyle;
+        $scope.temperatureDirtyState = $scope.dirtyValueStyle;
+    };
+
+    $scope.setHueSaturationDirty = function() {
+        $scope.temperatureDirtyState = $scope.currentValueStyle;
+        $scope.hueSaturationDirtyState = $scope.dirtyValueStyle;
     };
 
     $scope.setHue = function(val) {
         $scope.hueValue = val;
-        $scope.setAll($scope.user, {hue : $scope.hueValue}, $scope.lightCount);
+        $scope.setAll($scope.user, {
+            hue: $scope.hueValue
+        }, $scope.lightCount);
+        $scope.setTemperatureDirty();
+
     };
 
     $scope.setSaturation = function(val) {
         $scope.saturation = val;
-        $scope.setAll($scope.user, {sat : $scope.saturation}, $scope.lightCount);
+        $scope.setAll($scope.user, {
+            sat: $scope.saturation
+        }, $scope.lightCount);
+        $scope.setTemperatureDirty();
     };
 });
 
 //Extra directive to create an html range input that binds its value to an angular scope variable.
 //(Needed only for IE -- other browers do it natively)
-hueNgApp.directive("rangetemp", function () {
+hueNgApp.directive("rangetemp", function() {
     "use strict";
     return {
         restrict: "E",
-        template: '<input id ="tempRange"  ng-disabled="isDisabled" type="range" title="Change color temperature" min="2000" max="6500" value="4000" ng-model="colorTemperature" style="width:100px; display:inline"/>',
-        link: function (scope, element) {
+        template: '<input id ="tempRange" ng-disabled="isDisabled" type="range" title="Change color temperature" min="2000" max="6500" value="4000" ng-model="colorTemperature" style="width:100px; display:inline"/>',
+        link: function(scope, element) {
             var rangeControl = element.find("input");
-            rangeControl.bind("change", function () {
-                scope.$apply(function () {
+            rangeControl.bind("change", function() {
+                scope.$apply(function() {
                     scope.colorTemperature = rangeControl.val();
                     scope.setCustomTemperature(scope.colorTemperature);
                 });
@@ -170,15 +255,15 @@ hueNgApp.directive("rangetemp", function () {
     };
 });
 
-hueNgApp.directive("rangesaturation", function () {
+hueNgApp.directive("rangesaturation", function() {
     "use strict";
     return {
         restrict: "E",
         template: '<input id="saturationRange" ng-disabled="isDisabled" type="range" title="Change brightness" min="0" max="255" value="180" ng-model="saturation" style="width:100px; display:inline"/>',
-        link: function (scope, element) {
+        link: function(scope, element) {
             var rangeControl = element.find("input");
-            rangeControl.bind("change", function () {
-                scope.$apply(function () {
+            rangeControl.bind("change", function() {
+                scope.$apply(function() {
                     scope.saturation = Math.round(rangeControl.val());
                     scope.setSaturation(scope.saturation);
                 });
@@ -187,15 +272,15 @@ hueNgApp.directive("rangesaturation", function () {
     };
 });
 
-hueNgApp.directive("rangehue", function () {
+hueNgApp.directive("rangehue", function() {
     "use strict";
     return {
         restrict: "E",
-        template: '<input id="saturationHue" ng-disabled="isDisabled" type="range" title="Change hue" min="0" max="65535" value="0" ng-model="hueValue" style="width:100px; display:inline"/>',
-        link: function (scope, element) {
+        template: '<input id="saturationHue" ng-disabled="isDisabled" type="range" title="Change hue" min="0" max="65535" value="0" ng-model="hueValue" style="width:50px; display:inline"/>',
+        link: function(scope, element) {
             var rangeControl = element.find("input");
-            rangeControl.bind("change", function () {
-                scope.$apply(function () {
+            rangeControl.bind("change", function() {
+                scope.$apply(function() {
                     scope.hueValue = Math.round(rangeControl.val());
                     scope.setHue(scope.hueValue);
                 });
@@ -204,15 +289,15 @@ hueNgApp.directive("rangehue", function () {
     };
 });
 
-hueNgApp.directive("rangebright", function () {
+hueNgApp.directive("rangebright", function() {
     "use strict";
     return {
         restrict: "E",
         template: '<input id="brightnessRange" ng-disabled="isDisabled" type="range" title="Change brightness" min="0" max="255" value="180" ng-model="brightness" style="width:100px; display:inline"/>',
-        link: function (scope, element) {
+        link: function(scope, element) {
             var rangeControl = element.find("input");
-            rangeControl.bind("change", function () {
-                scope.$apply(function () {
+            rangeControl.bind("change", function() {
+                scope.$apply(function() {
                     scope.brightness = Math.round(rangeControl.val());
                     scope.setBrightness(scope.brightness);
                 });
@@ -221,5 +306,19 @@ hueNgApp.directive("rangebright", function () {
     };
 });
 
-
-
+hueNgApp.directive("colorpreview", function() {
+    "use strict";
+    return {
+        restrict: "E",
+        template: '<input id="colorPreview" type="range" title="Change color" min="0" max="16777215" value="0" ng-model="color" style="width:100px; display:inline"/>',
+        link: function(scope, element) {
+            var rangeControl = element.find("input");
+            rangeControl.bind("input", function() {
+                scope.$apply(function() {
+                    scope.color = Math.round(rangeControl.val());
+                    scope.setColor(scope.color);
+                });
+            });
+        }
+    };
+});
